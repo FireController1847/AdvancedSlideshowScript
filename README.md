9aa530a5b41039b1848432eb875e017eacfac339
FireController1847 <usfirepilot123@gmail.com>
2021-05-10 03:10:38 -0600

    Major Changes

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # AdvancedSlideshowScript
 An advanced slideshow/carousel JavaScript script for webpages.
 
-See example.js / example.html for an example usage.
+# LICENSE EXCEPTIONS
+There is a DIFFERENCE LICENSE for the following files:
+
+example/example.css
+
+The LICENSE to be used for this file is:
+
+"All rights reserved. Do not distribute."
+
+Please come up with your own CSS for your own slideshow!
---
+++ example/example.js
@@ -0,0 +1,19 @@
+﻿var primarySlideshow;
+
+window.onload = function() {
+    primarySlideshow = new Slideshow(
+        Slideshow.getItemsFromElement("quote-slideshow", "slideshow-item"),
+        Slideshow.getItemsFromElement("quote-slideshow-indicators", "slideshow-indicator"),
+        true,
+        true,
+        true,
+        7500,
+        10000,
+        "slideshow-indicator-selected",
+        "slideshow-item-prev",
+        "slideshow-item-selected",
+        "slideshow-item-next",
+        "slideshow-item-transitioning",
+        "slideshow-item-notransition"
+    );
+}
